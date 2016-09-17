0c64f17480e7a2bf6f5334d48d0abee403ca632f
LSQ-PC\LSQ <823725861@qq.com>
2016-09-17 22:34:25 +0800

    没有命令行参数

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log("HelloWorld");
+function Printx(x:number):void{
+    let i,j:number=0;
+    
+    for(i=0;i<x;i++){
+        let t :string="";
+        for(j=0;j<x+i;j++){
+            if(j<x-i-1){
+                t+=" ";
+            }
+            else{
+                t+="*";
+            }
+        }
+        console.log(t);
+    }
+}
+let x:number=5;
+
+Printx(x);
